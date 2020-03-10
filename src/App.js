4c899ab8664179013335b4af1209cfa204aaab25
Luiz Nunes <l.n.almeida.ti@gmail.com>
2020-03-09 22:53:32 -0300

    Componente VideoList estático adicionado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
+import VideoList from './components/VideoList'
 
 function App () {
   return (
     <div className='App'>
       <h1>Olá Mundo!</h1>
+      <VideoList />
     </div>
   )
 }
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -4,8 +4,16 @@ function VideoList () {
   return (
     <ul className='video-list'>
       <li className='video'>
-        <img />
-        <p>Nome</p>
+        <img src='https://storage.coverr.co/thumbnails/coverr-sparks-of-bonfire-1573980240958' alt='Chamas' />
+        <p>Chamas</p>
+      </li>
+      <li className='video'>
+        <img src='https://storage.coverr.co/thumbnails/coverr-mirror-road-view-1575200654885' alt='Retrovisor' />
+        <p>Retrovisor</p>
+      </li>
+      <li className='video'>
+        <img src='https://storage.coverr.co/thumbnails/Golden_Gate_Traffic' alt='Golden Gate' />
+        <p>Golden Gate</p>
       </li>
     </ul>
   )
