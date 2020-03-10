7d921b1f495ec52a765a2392106e3904d9322b65
Luiz Nunes <l.n.almeida.ti@gmail.com>
2020-03-09 22:29:16 -0300

    Criado componente VideoList

---
+++ src/components/VideoList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function VideoList () {
+  return (
+    <ul className='video-list'>
+      <li className='video'>
+        <img />
+        <p>Nome</p>
+      </li>
+    </ul>
+  )
+}
+
+export default VideoList
