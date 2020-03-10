b319104cd9bda9186dd3fbda720f99589c79de8d
Luiz Nunes <l.n.almeida.ti@gmail.com>
2020-03-09 23:23:00 -0300

    Add componentes estáticos VideoInline  VideoCinema

---
+++ src/components/VideoCinema.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// import { Container } from './styles';
+
+function VideoCinema (props) {
+  return (
+    <div className='video-cinema'>
+      {props.children}
+    </div>
+  )
+}
+export default VideoCinema
---
+++ src/components/VideoInline.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// import { Container } from './styles';
+
+function VideoInline (props) {
+  return (
+    <div className='video-inline'>
+      {props.children}
+    </div>
+  )
+}
+export default VideoInline
