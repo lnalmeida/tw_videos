3ddc0eef24d15ed9f325e121060c422739a42fe4
Luiz Nunes <l.n.almeida.ti@gmail.com>
2020-03-11 22:56:02 -0300

    Criados os serviçoes da api

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 class VideoPlayer extends Component {
   render () {
-      const { video } = this.props
+    const { video } = this.props
     return (
       <div className='video-player'>
         <video
